1b276849f87f2ef45077cf75583e571fd9c0ed0a
andiharun29 <andiharunalrasyid14@gmail.com>
2024-11-22 15:46:02 +0700

    memasukkan list mata kuliah yang ingin di tampilkan

---
+++ app/src/main/java/com/example/navigationmultipledata/data/MataKuliah.kt
@@ -0,0 +1,13 @@
+package com.example.navigationmultipledata.data
+
+object MataKuliah {
+    val options = listOf(
+        "Agama Islam dan Kemuhammadiyahan",
+        "Pengembangan Aplikasi Web",
+        "Pancasila",
+        "Dasar teknologi Informasi",
+        "Fisika dan Elektronika",
+        "Logika Teknik Pemrograman"
+
+    )
+}
